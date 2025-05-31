c4db71e049b7ef9e34664855b75058f96f01ecd3
Voidbroexec <voidbro.exec@gmail.com>
2025-05-31 02:55:34 +0200

    Update vite.config.ts

--- template_for_threejs(your-websitein3d)/vite.config.ts
+++ template_for_threejs(your-websitein3d)/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  server: {
-    open: true
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   }
-}); 
+})
