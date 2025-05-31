e9ab17ad48c303cee8429af3a530a3bb3dc33871
Voidbroexec <voidbro.exec@gmail.com>
2025-05-31 03:27:51 +0200

    Update vite.config.js

--- site3d/vite.config.ts
+++ site3d/vite.config.ts
@@ -1,10 +1,14 @@
-build: {
-  rollupOptions: {
-    output: {
-      manualChunks: {
-        three: ['three'],
-        tween: '@tweenjs/tween.js'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+          tween: ['@tweenjs/tween.js']
+        }
       }
     }
   }
-}
+})
